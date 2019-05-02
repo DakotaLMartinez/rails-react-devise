250768b4178013b6faa6381f14ce8a509e7727a7
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-05-02 16:28:19 -0700

    separate login logic from react

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/home'
+  get 'welcome/home', as: 'home'
+  get 'app', to: 'welcome#react', as: 'react'
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
